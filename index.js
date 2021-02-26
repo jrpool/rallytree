de687a1b1d28a9787be91d89b4937509a363797c
Jonathan Pool <jonathan.pool@cvshealth.com>
2021-02-26 18:16:37 -0500

    Modularized score

--- index.js
+++ index.js
@@ -530,141 +530,6 @@ const copyTree = (storyRefs, parentType, parentRef) => {
     return Promise.resolve('');
   }
 };
-// ==== SCORING OPERATION ====
-// Recursively acquires test results from a tree of user stories.
-const scoreTree = storyRef => {
-  // Get data on the user story.
-  getItemData(storyRef, [], ['Children', 'TestCases'])
-  .then(
-    // When the data arrive:
-    data => {
-      if (! globals.isError) {
-        // Get data on the test cases of the user story, if any.
-        getCollectionData(
-          data.testCases.count ? data.testCases.ref : '',
-          ['LastVerdict', 'Risk', 'Priority'],
-          ['Defects']
-        )
-        .then(
-          // When the data, if any, arrive:
-          cases => {
-            // Process the test cases in parallel.
-            cases.forEach(testCase => {
-              if (! globals.isError) {
-                const verdict = testCase.lastVerdict;
-                const riskWeight = globals.scoreWeights.risk[testCase.risk];
-                const priorityWeight = globals.scoreWeights.priority[testCase.priority];
-                const weight = Number.parseInt(riskWeight) + Number.parseInt(priorityWeight);
-                const defectsCollection = testCase.defects;
-                let newNumerator;
-                report([['total']]);
-                if (verdict === 'Pass') {
-                  newNumerator = totals.numerator + weight;
-                  report([
-                    ['verdicts'],
-                    ['scoreVerdicts'],
-                    ['passes'],
-                    [
-                      'score',
-                      Math.round(
-                        newNumerator ? 100 * newNumerator / (totals.denominator + weight) : 0
-                      ) - totals.score
-                    ],
-                    ['numerator', weight],
-                    ['denominator', weight]
-                  ]);
-                }
-                else if (verdict === 'Fail') {
-                  newNumerator = totals.numerator;
-                  report([
-                    ['verdicts'],
-                    ['scoreVerdicts'],
-                    ['fails'],
-                    [
-                      'score',
-                      Math.round(
-                        newNumerator ? 100 * newNumerator / (totals.denominator + weight) : 0
-                      ) - totals.score
-                    ],
-                    ['denominator', weight]
-                  ]);
-                }
-                else if (verdict !== null) {
-                  report([['verdicts']]);
-                }
-                // If the test case has any defects:
-                /*
-                  NOTICE: this condition was liberalized temporarily in February 2021 because of a
-                  Rally bug that reports all defect counts as 0.
-                */
-                if (defectsCollection.count >= 0) {
-                  // Get data on the defects.
-                  getCollectionData(defectsCollection.ref, ['Severity'], [])
-                  .then(
-                    // When the data arrive:
-                    defects => {
-                      // Notify the user whether the defect count bug has been corrected.
-                      if (defects.length) {
-                        if (defectsCollection.count) {
-                          console.log(
-                            'Rally defect-count bug has been corrected! Revise scoreTree().'
-                          );
-                        }
-                        else {
-                          console.log('Rally defect-count bug not yet corrected!');
-                        }
-                      }
-                      report([['defects', defects.length]]);
-                      // Process their severities.
-                      const severities = defects
-                      .map(defect => defect.severity)
-                      .reduce((tally, score) => {
-                        tally[score]++;
-                        return tally;
-                      }, {
-                        'Minor Problem': 0,
-                        'Major Problem': 0
-                      });
-                      report([
-                        ['major', severities['Major Problem']],
-                        ['minor', severities['Minor Problem']]
-                      ]);
-                    },
-                    error => err(error, 'getting data on defects')
-                  );
-                }
-              }
-              else {
-                return;
-              }
-            });
-          },
-          error => err(error, `getting data on test cases ${data.testCases.ref}`)
-        );
-        // Get data on the child user stories of the user story, if any.
-        getCollectionData(data.children.count ? data.children.ref : '', [], [])
-        .then(
-          // When the data, if any, arrive:
-          children => {
-            // Process the children in parallel.
-            children.forEach(child => {
-              if (! globals.isError) {
-                const childRef = shorten(
-                  'hierarchicalrequirement', 'hierarchicalrequirement', child.ref
-                );
-                if (! globals.isError) {
-                  scoreTree(childRef);
-                }
-              }
-            });
-          },
-          error => err(error, 'getting data on child user stories')
-        );
-      }
-    },
-    error => err(error, 'getting data on user story')
-  );
-};
 // ==== OWNERSHIP CHANGE OPERATION ====
 // Sequentially ensures the ownership of an array of work items (tasks or test cases).
 const takeItems = (longItemType, shortItemType, items) => {
@@ -2312,6 +2177,7 @@ const requestHandler = (request, res) => {
   })
   .on('end', () => {
     const requestURL = request.url;
+    const op = {globals, totals, err, shorten, report, getItemData, getCollectionData};
     // METHOD GET: If the request requests a resource:
     if (method === 'GET') {
       // If the requested resource is a file, serve it.
@@ -2341,7 +2207,8 @@ const requestHandler = (request, res) => {
       }
       else if (requestURL === '/scoretally' && globals.idle) {
         streamInit();
-        scoreTree(globals.rootRef);
+        const scoreTree = require('scoreTree');
+        scoreTree(op, globals.rootRef);
       }
       else if (requestURL === '/taketally' && globals.idle) {
         streamInit();
